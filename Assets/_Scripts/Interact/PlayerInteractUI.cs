4751f276aee3f7d76d70fe25b967366582541922
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2024-06-11 01:00:53 +0300

    init

---
+++ Assets/_Scripts/Interact/PlayerInteractUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+public class PlayerInteractUI : MonoBehaviour {
+
+    [SerializeField] private GameObject containerGameObject;
+    [SerializeField] private PlayerInteract playerInteract;
+    //[SerializeField] private TextMeshProUGUI interactTextMeshProUGUI;
+
+    private void Update() {
+        if (playerInteract.GetInteractableObject() != null) {
+            Show(playerInteract.GetInteractableObject());
+        } else {
+            Hide();
+        }
+    }
+
+    private void Show(IInteractable interactable) {
+        containerGameObject.SetActive(true);
+        UItoolkit_unarchive(interactable.GetInteractText());
+        //interactTextMeshProUGUI.text = interactable.GetInteractText();
+    }
+
+    private void Hide() {
+        containerGameObject.SetActive(false);
+    }
+    private void UItoolkit_unarchive(string text)
+    {
+        UIDocument document = containerGameObject.GetComponent<UIDocument>();
+        VisualElement visualElement = document.rootVisualElement;
+        Label label = visualElement.Q("Label") as Label;
+        label.text = text;
+    }
+
+}
