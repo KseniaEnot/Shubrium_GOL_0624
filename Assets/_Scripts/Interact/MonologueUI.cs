88eb4a42734b9e9c17289e0d4a9bdecdbe65c3c3
KseniaEnot <58525970+KseniaEnot@users.noreply.github.com>
2024-06-16 19:39:11 +0300

    stuff

--- Assets/_Scripts/Interact/MonologueUI.cs
+++ Assets/_Scripts/Interact/MonologueUI.cs
@@ -58,7 +58,7 @@ public class MonologueUI : MonoBehaviour, IInteractable
     {
         if (!isRunning)
         {
-            Player.instance.OnDialogInteract(false, false);
+            Player.instance.OnDialogInteract(false, true);
             sentences = new Queue<string>();
             sentences.Clear();
             Debug.Log("StartDialogue " + gameObject.name);
